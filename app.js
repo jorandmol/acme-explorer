4d61ab551c3b1029a4ad02cf102a0a1eb8272994
carnuare <carnuare@gmail.com>
2023-01-24 20:49:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import initMongoDBConnection from './api/config/mongoose.js'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8080
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// welcome route
+app.get('/', function (req, res) {
+    res.send('Welcome to ACME-Explorer RESTful API')
+})
+
+try{
+    await initMongoDBConnection()
+    app.listen(port, function () {
+      console.log('ACME-Explorer RESTful API server started on: ' + port)
+    })
+  }
+  catch(err){
+    console.error('ACME-Explorer RESTful API could not connect to DB ' + err)
+  }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# ACME Explorer API
+Componentes del grupo:
+ - Jorge Andrea Molina
+ - Juan Carlos Cortés Muñoz
+ - Carlos Núñez Arenas
