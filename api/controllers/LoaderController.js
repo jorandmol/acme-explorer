9ec5aec3c8c45913a1d13be0da0d6f90ce606d5a
jorandmol <jorandmol.dev@gmail.com>
2023-02-07 20:49:02 +0100

    Refactor controller

--- api/controllers/LoaderController.js
+++ api/controllers/LoaderController.js
@@ -2,41 +2,22 @@ import mongoose from 'mongoose'
 import { readFile } from 'fs/promises';
 
 const storeJsonInsertMany = async (req, res) => {
-  let mongooseModel = null
-  let sourceFile = null
-  let response = ''
-  if (req.query.mongooseModel && req.query.sourceFile) {
-    mongooseModel = req.query.mongooseModel
-    sourceFile = req.query.sourceFile
-    
+  const { model, sourceFile } = req.query
+  if (!model || !sourceFile) {
+    res.status(400).send('Model and sourceFile are mandatory fields')
+  }
 
-    console.log('Modelo:'+mongooseModel)
-    console.log('sourceFile:'+sourceFile)
+  const data = JSON.parse(await readFile(new URL(sourceFile, import.meta.url)));
+  const collectionModel = mongoose.model(model)
 
-    const json = JSON.parse(await readFile(new URL(sourceFile, import.meta.url)));
-
-    const collectionModel = mongoose.model(mongooseModel)
-
-    console.log('inserting the json from file: ' + sourceFile + ', into the Model: ' + mongooseModel)
-  
-    try {
-      response = await collectionModel.insertMany(json, { ordered: false })
-      const responseFinal = 'All documents stored in the collection!'
-        console.log(responseFinal)
-        res.send(response)
-    }
-    catch (err) {
-      console.log(err)
-      res.send(err)
-    }
-
-  } else {
-    if (req.query.mongooseModel == null) response += 'A mandatory mongooseModel parameter is missed.\n'
-    if (req.query.sourceFile == null) response += 'A mandatory sourceFile parameter is missed.\n'
-    console.log(response)
-    res.send(response)
+  console.log('inserting the json from file: ' + sourceFile + ', into the Model: ' + model)
+  try {
+    const response = await collectionModel.insertMany(data, { ordered: false })
+    res.send(`A list of ${response.length} ${model}s have been inserted to the DB`)
+  }
+  catch (err) {
+    res.status(500).send(err)
   }
 }
-
 
 export { storeJsonInsertMany }
