9e99946a611d8f66ee6321766467b7dbf1aa54c6
Juan Carlos Cortés Muñoz <juacormun@alum.us.es>
2023-02-15 19:48:32 +0100

    Add application generator, improve actor generator

--- loaders/generators/actorGenerator.js
+++ loaders/generators/actorGenerator.js
@@ -1,18 +1,24 @@
-JG.repeat(500, 500, {
-    _id: JG.objectId(),
-    name: JG.firstName(),
-    surname: JG.lastName(),
-    password: 'test1234',
-    email() {
-      return (
-        _.snakeCase(this.name) +
-        this.surname +
-        '@' +
-        'gmail.com'
-      ).toLowerCase();
-    },
-    phone: '+43 634343434',
-    address: `${JG.integer(1, 100)} ${JG.street()}, ${JG.city()}, ${JG.state()}`,
-	role: JG.random('explorer', 'manager', 'sponsor', 'administrator'),
-  	ban: null
+JG.repeat(1000, 1000, {
+  _id: JG.objectId(),
+  name: JG.firstName(),
+  surname: JG.lastName(),
+  password: JG.guid(),
+  email() {
+    return (
+      _.snakeCase(this.name) +
+      this.surname +
+      '@' +
+      'gmail.com'
+    ).toLowerCase();
+  },
+  phone: `+${JG.integer(1, 999)} ${JG.integer(100000000, 999999999)}`,
+  address: `${JG.integer(1, 100)} ${JG.street()}, ${JG.city()}, ${JG.state()}`,
+  role: JG.random('explorer', 'manager', 'sponsor', 'administrator'),
+  ban: JG.random(
+    null,
+    {
+      date: JG.date(new Date(2022, 0, 1)),
+      reason: JG.random('Bad use', 'Bad language', 'Fraud', 'Spam')
+    }
+  )
 });
