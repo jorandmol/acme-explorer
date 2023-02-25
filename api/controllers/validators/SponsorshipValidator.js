402e362ec2b70a4dec3a10c968d1650305895efe
jorandmol <jorandmol.dev@gmail.com>
2023-02-25 10:29:53 +0100

    Fix reference error

--- api/controllers/validators/SponsorshipValidator.js
+++ api/controllers/validators/SponsorshipValidator.js
@@ -1,14 +1,14 @@
 import { check } from 'express-validator'
 
-const creationValidator = [
-  check('tripId').exists({ checkFalsy: true }).isMongoId(),
-  ...updateValidator
-]
-
 const updateValidator = [
   check('sponsor').exists({ checkFalsy: true }).isMongoId(),
   check('banner').exists({ checkFalsy: true }).isBase64(),
   check('link').exists({ checkFalsy: true }).isURL()
+]
+
+const creationValidator = [
+  check('tripId').exists({ checkFalsy: true }).isMongoId(),
+  ...updateValidator
 ]
 
 export { creationValidator, updateValidator }
