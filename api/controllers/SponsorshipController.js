0d4c5022d67b5777291e164f621b99584acae632
jorandmol <jorandmol.dev@gmail.com>
2023-02-25 10:30:33 +0100

    Add list route to sponsorships service

--- api/controllers/SponsorshipController.js
+++ api/controllers/SponsorshipController.js
@@ -1,11 +1,67 @@
 import Trip from '../models/TripModel.js'
-import { ObjectId } from 'mongodb'
+import Actor from '../models/ActorModel.js'
+
+/*
+* TODO:
+* Extract the check-actor-sponsorship procedure, which is repeated in all functions
+* to a middleware so it's easier to maintain
+*/
+
+const listSponsorships = async (req, res) => {
+  // TODO: change this when auth is implemented
+  const { actorId } = req.headers
+  try {
+    const actor = await Actor.findById(actorId)
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+    if (actor.role !== RoleEnum.SPONSOR) {
+      res.status(403).send('Actor does not have the required role')
+      return
+    }
+
+    const sponshorships = await Trip.aggregate([
+      { $unwind: sponsorships },
+      { $match: {
+        "sponshorships.sponsor": actor._id 
+      }},
+      { $project: {
+        _id: "$sponsorships._id",
+        sponsor: "$sponsorships.sponsor",
+        banner: "$sponsorships.banner",
+        link: "$sponsorships.link",
+        paidAt: "$sponsorships.paidAt",
+        trip: {
+          _id: "$_id",
+          ticker: "$ticker",
+          title: "$title",
+          description: "$description"
+        }
+      }}
+    ])
+
+    res.json(sponshorships)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
 
 const createSponsorship = async (req, res) => {
-  // The sponsor id comes from the logged user
-  const sponsor = new ObjectId('63e144632ba413df4fa0d9b6')
+  // TODO: change this when auth is implemented
+  const { actorId } = req.headers
   const { tripId, banner, link } = req.body
   try {
+    const actor = await Actor.findById(actorId)
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+    if (actor.role !== RoleEnum.SPONSOR) {
+      res.status(403).send('Actor does not have the required role')
+      return
+    }
+
     const trip = await Trip.findById(tripId)
     if (!trip) {
       res.status(404).send('Trip not found')
@@ -39,7 +95,12 @@ const readSponsorship = async (req, res) => {
         banner: "$sponsorships.banner",
         link: "$sponsorships.link",
         paidAt: "$sponsorships.paidAt",
-        tripId: "$_id"
+        trip: {
+          _id: "$_id",
+          ticker: "$ticker",
+          title: "$title",
+          description: "$description"
+        }
       }}
     ])
     if (!sponsorships.length) {
@@ -52,15 +113,27 @@ const readSponsorship = async (req, res) => {
   }
 }
 
-// TODO: check if the sponsor logged in is the sponsorship creator
 const updateSponsorship = async (req, res) => {
+  // TODO: change this when auth is implemented
   const { id } = req.params
+  const { actorId } = req.headers
   const newSponsorship = req.body
   try {
+    const actor = await Actor.findById(actorId)
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+    if (actor.role !== RoleEnum.SPONSOR) {
+      res.status(403).send('Actor does not have the required role')
+      return
+    }
+
     const sponsorships = await Trip.aggregate([
       { $unwind: sponsorships },
       { $match: {
-        "sponshorships._id": id 
+        "sponshorships._id": id,
+        "sponshorships.sponsor": actor._id 
       }},
       { $project: {
         _id: "$sponsorships._id",
@@ -87,20 +160,32 @@ const updateSponsorship = async (req, res) => {
     const prevSponsorships = [ ...trip.sponsorships ].filter(s => s._id !== sponsorship._id)
     const newSponsorships = [ ...prevSponsorships, newSponsorship ]
     const updatedTrip = await Trip.findOneAndUpdate({ _id: sponsorship.tripId }, { $set: {"sponsorships.$": newSponsorships }}, { new: true })
-    res.json({ message: 'Sponsorship successfully deleted from trip ' + updatedTrip._id })
+    res.json(updatedTrip)
   } catch (err) {
     res.status(500).send(err)
   }
 }
 
-// TODO: check if the sponsor logged in is the sponsorship creator
 const paySponsorship = async (req, res) => {
+  // TODO: change this when auth is implemented
   const { id } = req.params
+  const { actorId } = req.headers
   try {
+    const actor = await Actor.findById(actorId)
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+    if (actor.role !== RoleEnum.SPONSOR) {
+      res.status(403).send('Actor does not have the required role')
+      return
+    }
+
     const sponsorships = await Trip.aggregate([
       { $unwind: sponsorships },
       { $match: {
-        "sponshorships._id": id 
+        "sponshorships._id": id,
+        "sponshorships.sponsor": actor._id 
       }},
       { $project: {
         _id: "$sponsorships._id",
@@ -128,20 +213,32 @@ const paySponsorship = async (req, res) => {
     const prevSponsorships = [ ...trip.sponsorships ].filter(s => s._id !== sponsorship._id)
     const newSponsorships = [ ...prevSponsorships, sponsorship ]
     const updatedTrip = await Trip.findOneAndUpdate({ _id: sponsorship.tripId }, { $set: {"sponsorships.$": newSponsorships }}, { new: true })
-    res.json({ message: 'Sponsorship successfully deleted from trip ' + updatedTrip._id })
+    res.json(updatedTrip)
   } catch (err) {
     res.status(500).send(err)
   }
 }
 
-// TODO: check if the sponsor logged in is the sponsorship creator
 const deleteSponsorship = async (req, res) => {
+  // TODO: change this when auth is implemented
   const { id } = req.params
+  const { actorId } = req.headers
   try {
+    const actor = await Actor.findById(actorId)
+    if (!actor) {
+      res.status(404).send('Actor not found')
+      return
+    }
+    if (actor.role !== RoleEnum.SPONSOR) {
+      res.status(403).send('Actor does not have the required role')
+      return
+    }
+
     const sponsorships = await Trip.aggregate([
       { $unwind: sponsorships },
       { $match: {
-        "sponshorships._id": id 
+        "sponshorships._id": id,
+        "sponshorships.sponsor": actor._id 
       }},
       { $project: {
         _id: "$sponsorships._id",
@@ -167,10 +264,10 @@ const deleteSponsorship = async (req, res) => {
 
     const newSponsorships = [ ...trip.sponsorships ].filter(s => s._id !== sponsorship._id)
     const updatedTrip = await Trip.findOneAndUpdate({ _id: sponsorship.tripId }, { $set: {"sponsorships.$": newSponsorships }}, { new: true })
-    res.json({ message: 'Sponsorship successfully deleted from trip ' + updatedTrip._id })
+    res.json({ message: 'Sponsorship successfully deleted from trip ' + updatedTrip.ticker })
   } catch (err) {
     res.status(500).send(err)
   }
 }
 
-export { createSponsorship, readSponsorship, updateSponsorship, paySponsorship, deleteSponsorship}
+export { listSponsorships, createSponsorship, readSponsorship, updateSponsorship, paySponsorship, deleteSponsorship}
