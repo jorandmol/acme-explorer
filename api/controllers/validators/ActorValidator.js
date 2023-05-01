826740ac3ba08f35a8d610cb1dfecbfd501d9b6f
jorandmol <jorandmol.dev@gmail.com>
2023-05-01 15:20:40 +0200

    Fix actor validation

--- api/controllers/validators/ActorValidator.js
+++ api/controllers/validators/ActorValidator.js
@@ -5,8 +5,8 @@ const creationValidator = [
   check('surname').exists({ checkFalsy: true }).isString().isLength({ min: 2, max: 20 }).trim().escape(),
   check('email').exists({ checkFalsy: true }).isEmail().isLength({ min: 5, max: 50 }).trim().escape(),
   check('password').exists({ checkFalsy: true }).isString().isLength({ min: 8, max: 20 }).trim().escape(),
-  check('phone').optional().isString().isLength({ min: 9, max: 20 }).trim().escape(),
-  check('address').optional().isString().isLength({ min: 5, max: 50 }).trim().escape(),
+  check('phone').optional({checkFalsy: true}).isString().isLength({ min: 9, max: 20 }).trim().escape(),
+  check('address').optional({checkFalsy: true}).isString().isLength({ min: 5, max: 50 }).trim().escape(),
   check('role').exists({ checkFalsy: true }).isString().isLength({ min: 5, max: 20 }).trim().escape(),
   check('ban.date').optional().isISO8601().toDate(),
   check('ban.reason').optional().isString().isLength({ min: 5, max: 50 }).trim().escape()
